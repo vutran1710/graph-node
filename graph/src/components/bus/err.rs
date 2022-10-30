7c26e49f391a0464373e645243f1cfaefe5d3831
vutran1710 <me@vutr.io>
2022-10-30 08:42:06 +0700

    up

--- graph/src/components/bus/err.rs
+++ graph/src/components/bus/err.rs
@@ -1,5 +1,21 @@
+use crate::prelude::thiserror::Error;
+use std::fmt::Display;
+
 #[derive(Error, Debug)]
 pub enum BusError {
     SendMappingError(String),
     SendModificationError(String),
+}
+
+impl Display for BusError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            BusError::SendMappingError(err) => {
+                write!(f, "BusError: sending mapping failed => {:?}", err)
+            }
+            BusError::SendModificationError(err) => {
+                write!(f, "BusError: sending modifications failed => {}", err)
+            }
+        }
+    }
 }
--- graph/src/components/bus/traits.rs
+++ graph/src/components/bus/traits.rs
@@ -1,8 +1,10 @@
 use super::*;
+use crate::prelude::Logger;
+use async_trait::async_trait;
 
 #[async_trait]
 pub trait Bus: Send + Sync + 'static {
-    pub fn new(connection_uri: String) -> Bus;
-    pub async fn send_mapping_data() -> Result<(), BusError::SendMappingError>;
-    pub async fn send_modification_data() -> Result<(), BusError::SendModificationError>;
+    fn new(connection_uri: String, logger: Logger) -> Self;
+    async fn send_mapping_data() -> Result<(), BusError>;
+    async fn send_modification_data() -> Result<(), BusError>;
 }
