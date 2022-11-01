1dc55355c1c0cf1a1e6bc62c6da34f0cd3705e18
vutran1710 <me@vutr.io>
2022-11-02 00:31:39 +0700

    clean up trait

--- bus/rabbitmq/src/lib.rs
+++ bus/rabbitmq/src/lib.rs
@@ -2,9 +2,7 @@ use amiquip::{Connection, ExchangeDeclareOptions, ExchangeType, Publish};
 pub use async_trait::*;
 use graph::components::bus::Bus;
 use graph::components::bus::BusError;
-use graph::prelude::BlockPtr;
 use graph::prelude::DeploymentHash;
-use graph::prelude::EntityModification;
 use graph::prelude::Logger;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -26,9 +24,11 @@ impl Bus for RabbitmqBus {
             logger,
         }
     }
+
     fn get_name(&self) -> &str {
         self.name.as_str()
     }
+
     fn send_plain_text(&self, text: String, subgraph_id: DeploymentHash) -> Result<(), BusError> {
         let data_as_bytes = text.as_bytes();
         let routing_key = subgraph_id.as_str();
@@ -57,17 +57,5 @@ impl Bus for RabbitmqBus {
             },
             Err(e) => Err(BusError::SendPlainTextError(e.to_string())),
         };
-    }
-
-    async fn send_trigger_data(&self) -> Result<(), BusError> {
-        Ok(())
-    }
-    async fn send_modification_data(
-        &self,
-        _block_ptr: BlockPtr,
-        _mods: Vec<EntityModification>,
-        _manifest_idx_and_names: Vec<(u32, String)>,
-    ) -> Result<(), BusError> {
-        Ok(())
     }
 }
--- graph/src/components/bus/traits.rs
+++ graph/src/components/bus/traits.rs
@@ -1,7 +1,5 @@
 use super::*;
-use crate::prelude::BlockPtr;
 use crate::prelude::DeploymentHash;
-use crate::prelude::EntityModification;
 use crate::prelude::Logger;
 use async_trait::async_trait;
 
@@ -12,13 +10,4 @@ pub trait Bus: Send + Sync + 'static {
     fn get_name(&self) -> &str;
 
     fn send_plain_text(&self, value: String, subgraph_id: DeploymentHash) -> Result<(), BusError>;
-
-    async fn send_trigger_data(&self) -> Result<(), BusError>;
-
-    async fn send_modification_data(
-        &self,
-        block_ptr: BlockPtr,
-        mods: Vec<EntityModification>,
-        manifest_idx_and_names: Vec<(u32, String)>,
-    ) -> Result<(), BusError>;
 }
