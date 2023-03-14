35ed207f19eeddfaf570eef18153d24c87be70a5
vutran1710 <me@vutr.io>
2023-03-15 00:23:45 +0700

    backfill missing subgraph-name using migration rather than manually

--- store/postgres/migrations/2023-03-13_add_subgraph_name_to_deployment_schemas/up.sql
+++ store/postgres/migrations/2023-03-13_add_subgraph_name_to_deployment_schemas/up.sql
@@ -1,2 +1,20 @@
-alter table public.deployment_schemas
-  add subgraph_name text;
+ALTER TABLE public.deployment_schemas
+    ADD COLUMN IF NOT EXISTS subgraph_name text;
+
+WITH t AS (
+    SELECT
+        subgraphs.subgraph.name AS name,
+        subgraphs.subgraph_version.deployment AS HASH
+    FROM
+        subgraphs.subgraph_version
+        INNER JOIN subgraphs.subgraph ON (subgraphs.subgraph_version.id = subgraphs.subgraph.current_version
+                OR subgraphs.subgraph_version.id = subgraphs.subgraph.pending_version)
+)
+UPDATE
+    public.deployment_schemas
+SET
+    subgraph_name = t.name
+FROM
+    t
+WHERE
+    subgraph = t.hash;
